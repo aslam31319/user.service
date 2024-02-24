a0cc487ed6c8ba654500cd417f035c80f7c56f5e
Aslu <aslam31319@gmail.com>
2024-02-24 21:31:41 +0530

    changes to work

--- src/main/java/org/open/codes/billing/user/service/UserServiceApplication.java
+++ src/main/java/org/open/codes/billing/user/service/UserServiceApplication.java
@@ -2,13 +2,13 @@ package org.open.codes.billing.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableWebFluxSecurity
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableReactiveMethodSecurity
 public class UserServiceApplication {
 
